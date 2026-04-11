7b253476f2457de64e7760b771bb4e7b8669d248
minaaaa3 <qianqingjingshang312@gmail.com>
2026-04-12 05:41:33 +0900

    サイトをシンプルな状態に戻す

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: "/api/index.py",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
